a4b5846171d1d023f91cfa41cc8211e92bbed203
GustavoGutierrez <gr.gustavo93@gmail.com>
2021-03-05 10:11:25 -0300

    Initial commit

---
+++ src/main/java/com/meli/project/quasar/OperationFireOfQuasarApplication.java
@@ -0,0 +1,13 @@
+package com.meli.project.quasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationFireOfQuasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationFireOfQuasarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/project/quasar/OperationFireOfQuasarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.project.quasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationFireOfQuasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
